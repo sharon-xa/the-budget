28627ed39d0dacef33510487ce11eeee137c3ad2
sharon <ali93456@gmail.com>
2024-03-14 05:43:06 +0300

    feat(logs): new way of displaying logs
    I replaced the old way of showing the logs in the app with a real table,
    this table have sorting and pagination

--- src/components/history/Logs.tsx
+++ src/components/history/Logs.tsx
@@ -1,9 +1,9 @@
-import Log from "./Log";
+import LogsTable from "./LogsTable";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 import { getAuthToken } from "../../utils/auth";
 import { findLastLog } from "../../helpers";
-import LogsTable from "./LogsTable";
+import { ColumnDef } from "@tanstack/react-table";
 
 const Logs = () => {
     const token = getAuthToken();
@@ -21,6 +21,25 @@ const Logs = () => {
         },
     });
 
+    const columns: ColumnDef<LogType>[] = [
+        {
+            header: "Date",
+            accessorKey: "date",
+        },
+        {
+            header: "Type",
+            accessorKey: "transactionType",
+        },
+        {
+            header: "Amount",
+            accessorKey: "transactionAmount",
+        },
+        {
+            header: "Description",
+            accessorKey: "description",
+        },
+    ];
+
     let content;
     if (isLoading) content = (<p className="text-center">Loading...</p>);
     if (isError) content = (<p className="text-center">Failed Fetching Logs</p>);
@@ -30,29 +49,14 @@ const Logs = () => {
 
         if (logs.length === 0)
             content = <p className="text-center">There are no logs yet...</p>
-        else
-            content = (
-                logs.map(log =>
-                    <Log
-                        key={log.ID}
-                        id={`${log.ID}`}
-                        history={log.date}
-                        moneyAmount={log.transactionAmount}
-                        message={log.description}
-                        transactionType={log.transactionType}
-                        deletable={log.deletable ? log.deletable : false}
-                    />
-                )
-            )
+        else if (logs.length > 0)
+            content = <LogsTable data={logs ? logs : []} columns={columns} />
     }
 
     return (
-        <>
-            <section className="bg-white bg-opacity-60 p-6 border-4 rounded-md flex flex-col gap-5 min-w-[900px] max-w-[1000px] max-h-[540px] overflow-y-auto">
-                {content}
-            </section>
-            <LogsTable />
-        </>
+        <section className="bg-white bg-opacity-60 p-6 border-4 rounded-md flex flex-col gap-5 min-w-[900px] max-w-[1000px] max-h-[540px] overflow-y-auto">
+            {content}
+        </section>
     )
 }
 
---
+++ src/components/history/LogsTable.tsx
@@ -0,0 +1,72 @@
+import { Table, TableOptions, flexRender, getCoreRowModel, getPaginationRowModel, getSortedRowModel, useReactTable } from "@tanstack/react-table";
+import type { ColumnDef } from "@tanstack/react-table";
+import { useState } from "react";
+
+type useReactTable = <TData extends LogType>(
+  options: TableOptions<TData>
+) => Table<TData>
+
+type ColumnSort = {
+  id: string,
+  desc: boolean,
+}
+
+type SortingState = ColumnSort[]
+
+const BasicTable = ({ data, columns }: { data: LogType[], columns: ColumnDef<LogType>[] }) => {
+
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting: sorting,
+    },
+    onSortingChange: setSorting,
+  })
+
+  return (
+    <div className="">
+      <table className="">
+
+        {/* Header */}
+        {table.getHeaderGroups().map(headerGroup => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map(header =>
+              <th key={header.id} onClick={header.column.getToggleSortingHandler()}>
+                {flexRender(header.column.columnDef.header, header.getContext())}
+                {
+                  header.column.getIsSorted() === false ? null : header.column.getIsSorted() === "asc" ? " ⬆️ " : " ⬇️ "
+                }
+              </th>
+            )}
+          </tr>
+        ))}
+
+        {/* Body */}
+        {table.getRowModel().rows.map(row =>
+          <tr key={row.id}>
+            {row.getVisibleCells().map(cell => (
+              <td key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        )}
+
+      </table>
+      <div>
+        <button onClick={() => table.setPageIndex(0)}>First page</button>
+        <button disabled={!table.getCanPreviousPage()} onClick={() => table.previousPage()}>Previous page</button>
+        <button disabled={!table.getCanNextPage()} onClick={() => table.nextPage()}>Next page</button>
+        <button onClick={() => table.setPageIndex(table.getPageCount() - 1)}>Last page</button>
+      </div>
+    </div>
+  )
+}
+
+export default BasicTable;
+
