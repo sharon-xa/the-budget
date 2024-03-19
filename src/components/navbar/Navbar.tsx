4e12b08853de910829ff19a31e8193f23e9082e5
sharon <ali93456@gmail.com>
2024-03-19 14:14:44 +0300

    style(navbar): add styles to the new navbar desgin

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,10 +3,12 @@ import { Form } from "react-router-dom";
 const Navbar = () => {
     return (
         <header className="flex items-center justify-center">
-            <div className="flex justify-between w-[100%] m-4 p-6 max-w-screen-2xl">
-                <div>Refine</div>
+            <div className="flex justify-between items-center w-[100%] max-w-screen-2xl text-white m-6">
+                <a href="http://localhost:5173">
+                    <h1 className="font-bungee text-5xl cursor-pointer">Refine</h1>
+                </a>
                 <Form action="/logout" method="post">
-                    <button className={""}> Logout </button>
+                    <button className="bg-grey font-comfortaa py-4 px-10"> Logout </button>
                 </Form>
             </div>
         </header>
