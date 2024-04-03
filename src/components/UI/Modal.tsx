58332145e9ca50b53d55849bc126730896f307f3
sharon <ali93456@gmail.com>
2024-04-03 17:45:31 +0300

    fix: make the modal accept 2 types of modal windows
    first one is form modal
    second one is a dialog modal

--- src/components/UI/Modal.tsx
+++ src/components/UI/Modal.tsx
@@ -3,17 +3,17 @@ import { ReactNode } from "react";
 
 type Props = {
     children: ReactNode | JSX.Element,
-    onClose: () => void
+    onClose: () => void,
+    type: "form" | "dialog",
 }
 
-function Modal({ onClose, children }: Props) {
+function Modal({ onClose, children, type }: Props) {
     const modalRoot = document.getElementById('modal-root') as HTMLDivElement;
-
 
     return createPortal(
         <>
             <div
-                className="fixed w-full h-screen z-30 bg-black bg-opacity-50 backdrop-blur-md drop-shadow-xl"
+                className={`fixed w-full h-screen ${type === "form" ? "z-30" : "z-50"} bg-black bg-opacity-50 backdrop-blur-md drop-shadow-xl`}
                 onClick={onClose}>
             </div>
             {children}
