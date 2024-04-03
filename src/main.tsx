9bfb167397f4c0c4ef2803cf20af700784f59812
sharon <ali93456@gmail.com>
2024-04-03 17:42:23 +0300

    chore: back to the local server for testing

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 import axios from 'axios'
 import { getAuthToken } from './utils/auth.ts'
 
-axios.defaults.baseURL = "https://the-budget-backend.onrender.com";
+axios.defaults.baseURL = "http://localhost:8080";
 axios.defaults.headers.common['Authorization'] = getAuthToken();
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
